70454b07b9c825f2ee3e4b3f9f686a49b68e4439
qhj <me@qhj.moe>
2020-03-06 19:27:41 +0800

    gradle configuration and other things

--- vim/.vimrc
+++ vim/.vimrc
@@ -1,7 +1,7 @@
 filetype plugin indent on
 syntax on
 set number
-"set relativenumber
+set relativenumber
 set tabstop=4
 set shiftwidth=4
 set softtabstop=4
---
+++ vscode/.config/Code/User/snippets/vetur/snippets/vue.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    ${0}
+  </div>
+</template>
+
+<script>
+export default {
+  name: '$TM_FILENAME_BASE'
+}
+</script>
+
+<style scoped>
+
+</style>
