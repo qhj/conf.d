5bff42543522d707e225a5bc7410e0513cfd8b85
qhj <me@qhj.moe>
2020-05-03 09:44:34 +0800

    Fira Code

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+(setq doom-font (font-spec :family "Fira Code" :size 17))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -51,6 +51,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+
 
 
 (use-package rime
