a51ecd9fe0e652572d670e0b4cac9afaa52b7e29
qhj <me@qhj.moe>
2022-08-17 16:50:36 +0800

    plugins: add lualine

--- neovim/.config/nvim/lua/qhj/plugins.lua
+++ neovim/.config/nvim/lua/qhj/plugins.lua
@@ -121,6 +121,12 @@ return packer.startup(function(use)
     }
   end }
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = [[require('lualine').setup()]]
+  }
+
   -- notify
   use 'rcarriga/nvim-notify'
 
